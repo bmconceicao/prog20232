d1bff2bc75bb7e1df27d82cfbe507baf68e69bc3
bmconceicao <142513824+bmconceicao@users.noreply.github.com>
2023-08-31 11:26:02 -0300

    Add files via upload

---
+++ andre/aula 5.py
@@ -0,0 +1,10 @@
+listaInterna = ["lista", 1] 
+compra = ["ceiça", 1234, 1.2, ["lista", 1], listaInterna]
+print(compra[3][1])
+compra [3] = 51
+print(compra[0: len(compra) : 2])
+
+lista = []
+tupla = ()
+dicionario = {}
+
